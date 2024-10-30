a18be316836b62f47817ef0610d8bc11b9fab361
Kaushal Krishna <kaushalkrishna011@gmail.com>
2024-10-30 09:40:47 +0530

    updated cors policy for ip

--- src/app.js
+++ src/app.js
@@ -2,54 +2,48 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
+import paymentRouter from "./routes/payment.routes.js";
+import appRouter from "./routes/app.routes.js";
 
 const app = express();
 
 /** Use this CORS for production */
-/*app.use(
+app.use(
   cors((req, callback) => {
-    const allowedOrigins = [
-      "https://billing.xyrocloud.in",
-      "http://13.127.170.65:80"
-    ];
-    const origin = req.headers.origin;
+    const allowedIPs = ["13.127.170.65"];
 
-    if (allowedOrigins.includes(origin)) {
+    const clientIP =
+      req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+      
+    if (allowedIPs.includes(clientIP)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   })
-);*/
+);
 
 /**CAUTION: Remove this for production */
-app.use(cors({
-  origin: "*",
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: "*",
+//   credentials: true,
+// }));
 
+// Configurations
 app.use(cookieParser());
-
 app.use(express.json({ limit: "16kb" }));
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
-app.use(express.static(path.join(__dirname, 'static')));
-
-import paymentRouter from "./routes/payment.routes.js";
-import appRouter from "./routes/app.routes.js";
-
-//app.get("/", (req, res) => {
-  //res.send("KryptonPay Server is running")
-//})
-
 app.get("/api/v1", (req, res) => {
-    res.status(200).json({message: "KryptonPay API is live", success: true})
+  res.status(200).json({ message: "KryptonPay API is live", success: true });
 });
 
+// Static Webpage
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "static")));
+
+// Routes
 app.use("/api/v1/payments", paymentRouter);
 app.use("/api/v1/app", appRouter);
 
